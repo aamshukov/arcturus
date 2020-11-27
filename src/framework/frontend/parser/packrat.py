7963dcfc291fac1d4d88af579eb0b9b368f28b1a
Arthur <aamshukov@gmail.com>
2020-11-26 21:35:13 -0500

    Packrat parser initial commit.

---
+++ src/framework/frontend/parser/packrat.py
@@ -0,0 +1,14 @@
+
+
+
+def main(args):
+    try:
+        pass
+    except Exception as ex:
+        print(ex)
+    return 1
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    # buggy microsoft cannot fix the crap --- sys.exit(main(sys.argv[1:]))
